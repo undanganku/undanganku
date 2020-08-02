476b5ec3781c5bb8a93335f9375b989e52beef96
Sukma Rangga Pradita Wijaya <46914976+sukmaranggapradeta@users.noreply.github.com>
2020-08-02 16:44:01 +0700

    Updates

---
+++ precache-manifest.f19bbb9a37495bd5712c0aaba9d37885.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d37dc682c92d824fe9fdf00f1dd4b58",
+    "url": "/undanganku/index.html"
+  },
+  {
+    "revision": "078d3510570953fa94d3",
+    "url": "/undanganku/static/css/2.9ff99f11.chunk.css"
+  },
+  {
+    "revision": "b6964d87ccecf2c64b48",
+    "url": "/undanganku/static/css/main.b62993c4.chunk.css"
+  },
+  {
+    "revision": "078d3510570953fa94d3",
+    "url": "/undanganku/static/js/2.468c8264.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/undanganku/static/js/2.468c8264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6964d87ccecf2c64b48",
+    "url": "/undanganku/static/js/main.0417fc2a.chunk.js"
+  },
+  {
+    "revision": "2b7917173c11a7075d75",
+    "url": "/undanganku/static/js/runtime-main.9fff4d69.js"
+  },
+  {
+    "revision": "9551dc36a8defd7cb18f5733affdeed3",
+    "url": "/undanganku/static/media/love.9551dc36.png"
+  }
+]);
